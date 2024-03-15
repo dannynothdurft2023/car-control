0120e1d152b67a89c6da593e774c21d4e28d0462
danny <danny.nothdurft@icloud.com>
2024-03-15 20:14:53 +0100

    erste API abfrage

---
+++ src/app/api/carinfo/route.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+export async function GET() {
+  await client.connect();
+  const database = client.db("car-control");
+  const colCar = database.collection("car");
+
+  try {
+    const getCarInfo = await colCar.find().toArray();
+
+    console.log(getCarInfo);
+
+    if (getCarInfo) {
+      return NextResponse.json({
+        success: true,
+        message: "Auto gefunden",
+        data: getCarInfo[0],
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,77 +1,49 @@
+"use client";
 import "@/styles/carinfo.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const CarInfoPage = () => {
+  const currentUrl =
+    typeof window !== "undefined" ? window.location.origin : "";
+  const [car, setCar] = useState({ Fahrzeug: "wird geladen" });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const getCarInfo = async () => {
+        try {
+          const response = await axios.get(`${currentUrl}/api/carinfo`);
+          if (response.data.success) {
+            setCar(response.data.data);
+          } else {
+            console.log("Etwas ist schief gelaufen");
+          }
+        } catch (error: any) {
+          console.error(error.message);
+        }
+      };
+      getCarInfo();
+    }, 1);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main>
       <h1>Fahrzeug Information</h1>
       <div className="cc-carinfo-ct">
         <table>
           <tbody>
-            <tr>
-              <td>Marke:</td>
-              <td>Seat</td>
-            </tr>
-            <tr>
-              <td>Model:</td>
-              <td>Arosa</td>
-            </tr>
-            <tr>
-              <td>Farbe:</td>
-              <td>Schwarz</td>
-            </tr>
-            <tr>
-              <td>Erstzulassung:</td>
-              <td>24.04.1998</td>
-            </tr>
-            <tr>
-              <td>Kennzeichen:</td>
-              <td>OL K 5562</td>
-            </tr>
-            <tr>
-              <td>Kilometerstand:</td>
-              <td>200.000</td>
-            </tr>
-            <tr>
-              <td>HU:</td>
-              <td>09.2024</td>
-            </tr>
-            <tr>
-              <td>FIN:</td>
-              <td>VSSZZZ6HZWW053347</td>
-            </tr>
-            <tr>
-              <td>HSN:</td>
-              <td>7593</td>
-            </tr>
-            <tr>
-              <td>TSN:</td>
-              <td>39200M9</td>
-            </tr>
-            <tr>
-              <td>Emissionsklasse:</td>
-              <td>D3</td>
-            </tr>
-            <tr>
-              <td>Kraftstoff:</td>
-              <td>Benzin</td>
-            </tr>
-            <tr>
-              <td>Hubraum:</td>
-              <td>999</td>
-            </tr>
-            <tr>
-              <td>KW:</td>
-              <td>37</td>
-            </tr>
-            <tr>
-              <td>PS:</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>Höchstgeschwindigkeit:</td>
-              <td>151</td>
-            </tr>
+            {Object.entries(car).map(([key, value]) => {
+              if (key === "_id") {
+                return null;
+              }
+              return (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{value}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
