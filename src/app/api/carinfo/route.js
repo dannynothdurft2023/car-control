b18a3f08f07f0ce1bb08be9d2d448d9c07de182e
danny <danny.nothdurft@sugarshape.de>
2024-03-22 10:16:23 +0100

    test

--- src/app/api/carinfo/route.js
+++ src/app/api/carinfo/route.js
@@ -2,25 +2,26 @@ import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const uri = process.env.MONGODB_URI;
-  const client = new MongoClient(uri);
+  console.log("Was soll das");
+  // const uri = process.env.MONGODB_URI;
+  // const client = new MongoClient(uri);
 
-  await client.connect();
-  const database = client.db("car-control");
-  const colCar = database.collection("car");
-  console.log("vor try");
-  try {
-    console.log("in Try");
-    const getCarInfo = await colCar.find().toArray();
+  // await client.connect();
+  // const database = client.db("car-control");
+  // const colCar = database.collection("car");
+  // console.log("vor try");
+  // try {
+  //   console.log("in Try");
+  //   const getCarInfo = await colCar.find().toArray();
 
-    if (getCarInfo) {
-      return NextResponse.json({
-        success: true,
-        message: "Auto gefunden",
-        data: getCarInfo[0],
-      });
-    }
-  } catch (error) {
-    return NextResponse.json({ message: error }, { status: 500 });
-  }
+  //   if (getCarInfo) {
+  //     return NextResponse.json({
+  //       success: true,
+  //       message: "Auto gefunden",
+  //       data: getCarInfo[0],
+  //     });
+  //   }
+  // } catch (error) {
+  //   return NextResponse.json({ message: error }, { status: 500 });
+  // }
 }
