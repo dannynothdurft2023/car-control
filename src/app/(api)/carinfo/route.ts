ee5a8cd6138e549b5e10e196f8105b70d8283a4b
danny <danny.nothdurft@sugarshape.de>
2024-03-22 10:58:03 +0100

    API Test

--- src/app/(api)/carinfo/route.ts
+++ src/app/(api)/carinfo/route.ts
@@ -1,27 +1,21 @@
-import { MongoClient, Collection, Db } from "mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  // const uri: string = process.env.MONGODB_URI || "";
-  // const client: MongoClient = new MongoClient(uri);
+import connectToDatabase from "@/lib/db";
 
-  // await client.connect();
-  // const database: Db = client.db("car-control");
-  // const colCar: Collection = database.collection("car");
+export async function GET() {
+  const collection = await connectToDatabase("car");
 
   try {
-    //const getCarInfo = await colCar.find().toArray();
+    const carInfo = await collection.find().toArray();
 
-    //if (getCarInfo) {
-
-    return NextResponse.json({
-      success: true,
-      message: "Auto gefunden",
-      data: "hey",
-      //data: getCarInfo[0],
-      status: 200,
-    });
-    // }
+    if (carInfo) {
+      return NextResponse.json({
+        success: true,
+        message: "Auto gefunden",
+        data: carInfo[0],
+        status: 200,
+      });
+    }
   } catch (error) {
     return NextResponse.json({ message: error }, { status: 500 });
   }
---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { MongoClient, Collection, Db } from "mongodb";
+
+async function connectToDatabase(cluster: string): Promise<Collection> {
+  const uri: string = process.env.MONGODB_URI || "";
+  const client: MongoClient = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database: Db = client.db("car-control");
+    const collection: Collection = database.collection(cluster);
+    return collection;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default connectToDatabase;
