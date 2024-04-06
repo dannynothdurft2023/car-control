40a767cd5dc56ca3f1496539d0f3e1d59b40c1e0
danny <danny.nothdurft@icloud.com>
2024-04-06 19:35:46 +0200

    debug cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: () => [
+    {
+      basePath: "/",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store",
+        },
+      ],
+    },
+  ],
+};
 
 export default nextConfig;
